bf4b7bbea9b255beb482733928a1234c1a9e9fee
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-06-09 21:44:11 +0300

    Update of models.

--- models/SwapPairInformation.js
+++ models/SwapPairInformation.js
@@ -1,3 +1,5 @@
+const { convertSPInfoFromDB } = require('../modules/fetchDataFromDB/utils/converterFromDB');
+const { DataBaseNotAvailable } = require('../modules/utils/customException');
 const ModelTemplate = require('./_Model');
 
 //TODO: Паша: написать аннотации для параметров функций (а то `information` не особо информативно)
@@ -14,15 +16,15 @@ class SwapPairInformation extends ModelTemplate {
                 type: this.CustomTypes.ID,
                 primaryKey: true
             },
-            swap_pair_address:      { type: addressType },
-            root_address:           { type: addressType },
-            token1_address:         { type: addressType },
-            token2_address:         { type: addressType },
-            lptoken_address:        { type: addressType },
-            wallet1_address:        { type: addressType },
-            wallet2_address:        { type: addressType },
+            swap_pair_address: { type: addressType },
+            root_address: { type: addressType },
+            token1_address: { type: addressType },
+            token2_address: { type: addressType },
+            lptoken_address: { type: addressType },
+            wallet1_address: { type: addressType },
+            wallet2_address: { type: addressType },
             lptoken_wallet_address: { type: addressType },
-            swap_pair_name:         { type: this.Types.STRING(200) }
+            swap_pair_name: { type: this.Types.STRING(200) }
         }
     }
 
@@ -52,8 +54,50 @@ class SwapPairInformation extends ModelTemplate {
     static async getRecordByAddress(address) {
         return SwapPairInformation.findOne({ where: { swap_pair_address: address } });
     }
-}
 
+
+    static async getSwapPairs(offset = 0, limit = 100) {
+        let swapPairs = [];
+        try {
+            swapPairs = await SwapPairInformation.findAll({
+                offset: offset,
+                limit: limit,
+                order: [
+                    ['id', 'DESC']
+                ]
+            });
+            swapPairs = swapPairs.map((element) => convertSPInfoFromDB(element.dataValues));
+        } catch (err) {
+            console.log(err);
+            throw new DataBaseNotAvailable('SwapPairInformation');
+        }
+        return swapPairs;
+    }
+
+    static async getSwapPairIdByAddress(swapPairAddress) {
+        let swapPairId = -1;
+        try {
+            swapPairId = await SwapPairInformation.findOne({ where: { swap_pair_address: swapPairAddress } });
+            swapPairId = swapPairId.dataValues.id;
+        } catch (err) {
+            console.log(err);
+            throw new DataBaseNotAvailable('SwapPairInformation');
+        }
+        return swapPairId;
+    }
+
+    static async getSwapPairIdByName(swapPairName) {
+        let swapPairId = -1;
+        try {
+            swapPairId = await SwapPairInformation.findOne({ where: { swap_pair_name: swapPairName } });
+            swapPairId = swapPairId.dataValues.id;
+        } catch (err) {
+            console.log(err);
+            throw new DataBaseNotAvailable('SwapPairInformation');
+        }
+        return swapPairId;
+    }
+}
 
 
 module.exports = SwapPairInformation;
