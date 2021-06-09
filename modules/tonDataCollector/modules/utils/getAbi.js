b95f66939019eac7561eeb51c9d3fe1b304ca927
Anton <ntn4work@gmail.com>
2021-06-09 13:25:00 +0300

    pathes fixed

--- modules/tonDataCollector/modules/utils/getAbi.js
+++ modules/tonDataCollector/modules/utils/getAbi.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
+const { dirname } = require('path');
+const path = require('path')
 
-module.exports = (contractName) => {
+// module.exports = (contractName) => {
+//     if (contractName == 'rootSwapPairContract') {
+//         return JSON.parse(fs.readFileSync('./abi/RootSwapPairContract.abi.json'));
+//     }
+//     if (contractName == 'swapPairContract') {
+//         return JSON.parse(fs.readFileSync('./abi/SwapPairContract.abi.json'));
+//     }
+//     return '';
+// }
+
+const _dname = dirname(__filename);
+const mainPath = path.join(_dname, '..', '..', 'abi');
+
+function getAbi(contractName) {
     if (contractName == 'rootSwapPairContract') {
-        return JSON.parse(fs.readFileSync('./abi/RootSwapPairContract.abi.json'));
+        const p = path.join(mainPath, 'RootSwapPairContract.abi.json');
+        return JSON.parse(fs.readFileSync(p));
     }
     if (contractName == 'swapPairContract') {
-        return JSON.parse(fs.readFileSync('./abi/SwapPairContract.abi.json'));
+        const p =  path.join(mainPath, 'SwapPairContract.abi.json');
+        return JSON.parse(fs.readFileSync(p));
     }
     return '';
 }
+
+module.exports = getAbi;
