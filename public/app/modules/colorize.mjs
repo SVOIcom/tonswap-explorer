f477976f6b015a0a4e53daa81fa9b2c5322d808f
Anton <ntn4work@gmail.com>
2021-06-17 21:49:44 +0300

    lil fix

--- public/app/modules/colorize.mjs
+++ public/app/modules/colorize.mjs
@@ -1,6 +1,4 @@
 $( ".colored" ).each(function() {
-
-    let change = Number($( this ).text().replace('%', ''));
-    change >= 0 ? $( this ).addClass("green") : $( this ).addClass("red"); 
-
+    let cls = parseFloat($(this).text()) > 0 ? "green" : "red";
+    $(this).addClass(cls);
   });
