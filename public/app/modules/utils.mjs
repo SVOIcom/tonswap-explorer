db853924746c998fd290a29e89e8153ff75d8222
Andrey Nedobylsky <andrey@izzz.io>
2021-06-18 13:16:23 +0200

    BigNumberChanges

--- public/app/modules/utils.mjs
+++ public/app/modules/utils.mjs
@@ -114,7 +114,10 @@ const utils = {
      * @returns {number}
      */
     numberToUnsignedNumber(num, decimals = 9) {
-        return Number(Number(num).toFixed(decimals).replace('.', ''))
+        if(decimals === 0) {
+            return BigNumber(num).toFixed(decimals);
+        }
+        return (BigNumber(num).toFixed(decimals).replace('.', ''))
     },
     /**
      * Raw unsigned number to js number
@@ -123,7 +126,10 @@ const utils = {
      * @returns {number}
      */
     unsignedNumberToSigned(num, decimals = 9) {
-        return Number(Number(Number(num) / Math.pow(10, decimals)).toFixed(9));
+        if(decimals === 0) {
+            return BigNumber(num).toFixed(decimals);
+        }
+        return BigNumber(num).div(Math.pow(10, decimals)).toFixed(decimals);
     },
     /**
      * Big number to big string
